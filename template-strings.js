3b46bbf9714f436d818ba9fa870870a252b60bfe
Alice Robinson <valkrysa@gmail.com>
2017-05-01 16:09:27 -0700

    Update template-strings.js

--- template-strings.js
+++ template-strings.js
@@ -5,3 +5,21 @@ function getMessage () {
   // return `The year is ${new Date().getFullYear()}`
 }
 getMessage()
+
+const device_id = 5
+const guid = 10
+const username = 'alice'
+
+// ES5
+//var data = '{"device_id": "' + device_id + '", "guid": "' + guid + '", "username": "' + username + '"}'
+
+// ES6
+let data = `{"device_id": "${device_id}", "guid": "${guid}", "username": "${username}"}`
+
+function doubleMessage(number) {
+  return `Your number doubled is ${2 * number}`
+}
+
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
